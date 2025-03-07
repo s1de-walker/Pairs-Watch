05c9c1195f2d7f325c91b79cfbb6f3c907133704
Sujay <s1dewalker23@gmail.com>
2025-03-07 14:47:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ if not error_flag:
     # Display R-squared in the first column
     col1.metric(label="R-Squared", value=f"{r_squared:.3f}")
     # Use Streamlit's built-in color formatting
-    col1.markdown(f":blue[Relationship Strength: {ticker1} explains {r_squared*100:.0f}% of the variation in {ticker2}]")
+    col1.markdown(f":grey[Relationship Strength: {ticker1} explains {r_squared*100:.0f}% of the variation in {ticker2}]")
     
     # Display OLS Beta in the second column
     col2.metric(label="OLS Beta", value=f"{beta:.3f}")
