9abc94118f3172c69b5508c2c7044fb549cc6b32
Sujay <s1dewalker23@gmail.com>
2025-03-07 17:22:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -232,10 +232,11 @@ if not error_flag:
     #-------------------------------------------------------------------------
     st.subheader("Relative Risk")
     #st.markdown("Rolling Volatility Ratio")
-    st.write(":grey[This should be less than the number of days for the selected time period above]")
+    
 
     # Numeric input for rolling window period selection (in days)
     rolling_window = st.number_input("Enter Rolling Window Period (in days)", min_value=1, max_value=365, value=30)
+    st.write("**:grey[This should be less than the number of days for the selected time period above]**")
 
     # Calculate rolling volatility for each stock
     rolling_volatility_ticker1 = returns[ticker1].rolling(window=rolling_window).std().dropna()
